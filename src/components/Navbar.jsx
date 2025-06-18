529655fd6b77513cfd7244e1f08cbcaeec25e50c
Clarence Panto <clardaing128@gmail.com>
2025-06-17 22:15:49 -0700

    finished wiring the routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import dashboardIcon from "../assets/icons/Icons.png";
 import monitorIcon from "../assets/icons/monitor-one.png";
 import groupIcon from "../assets/icons/peoples-two.png";
 import logoutIcon from "../assets/icons/Logout.png";
+import { NavLink } from "react-router-dom";
+import clsx from "clsx";
 
 function Navbar() {
   return (
@@ -16,19 +18,47 @@ function Navbar() {
       </div>
       <section className="mt-8 flex flex-col">
         <ul className="flex flex-col gap-5 text-center">
-          <li className="items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md">
-            <img src={monitorIcon} alt="monitorIcon" />
-          </li>
-          <li className=" items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md">
-            <img src={dashboardIcon} alt="dashboardIcon" />
-          </li>
-          <li className="items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md">
-            <img src={groupIcon} alt="groupIcon" />
-          </li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              clsx(isActive ? "bg-[#FA9564]" : "bg-white")
+            }
+          >
+            <li className="items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md ">
+              <img src={monitorIcon} alt="monitorIcon" />
+            </li>
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) =>
+              clsx(isActive ? "bg-[#FA9564]" : "bg-white")
+            }
+          >
+            <li className=" items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md">
+              <img src={dashboardIcon} alt="dashboardIcon" />
+            </li>
+          </NavLink>
+          <NavLink
+            to="/customers"
+            className={({ isActive }) =>
+              clsx(isActive ? "bg-[#FA9564]" : "bg-white")
+            }
+          >
+            <li className="items-center justify-center flex w-[56px] h-[56px] hover:bg-[#FA9564] cursor-pointer rounded-md">
+              <img src={groupIcon} alt="groupIcon" />
+            </li>
+          </NavLink>
         </ul>
-        <div className="w-[56px] h-[56px] items-center justify-center flex absolute bottom-15 hover:bg-[#FA9564] cursor-pointer rounded-md">
-          <img src={logoutIcon} alt="logoutIcon" className=" scale-110 " />
-        </div>
+        <NavLink
+          to="/logout"
+          className={({ isActive }) =>
+            clsx(isActive ? "bg-[#FA9564]" : "bg-white")
+          }
+        >
+          <div className="w-[56px] h-[56px] items-center justify-center flex absolute bottom-15 hover:bg-[#FA9564] cursor-pointer rounded-md">
+            <img src={logoutIcon} alt="logoutIcon" className=" scale-110 " />
+          </div>
+        </NavLink>
       </section>
     </nav>
   );
